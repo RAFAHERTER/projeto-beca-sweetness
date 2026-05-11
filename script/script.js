0663aae6bda7f6dcbe2555cff4dc8f4b5ba986d4
Rafael Herter <herter.rafaell@gmail.com>
2026-05-11 11:16:36 -0300

    Projeto BecaSweetness

---
+++ script/script.js
@@ -0,0 +1,12 @@
+
+function pedir(){
+
+let numero = "5519999072876"
+
+let mensagem = "Olá! Vim pelo site da Beca Sweetness e gostaria de fazer uma encomenda."
+
+let url = `https://wa.me/${numero}?text=${encodeURIComponent(mensagem)}`
+
+window.open(url,'_blank')
+
+}
