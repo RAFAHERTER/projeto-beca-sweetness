a8a80f22042b5c23a3a5a7333cde327aabd95e34
Rafael Herter <herter.rafaell@gmail.com>
2026-05-16 13:48:55 -0300

    Opção de filtro adicionada

--- script/script.js
+++ script/script.js
@@ -14,3 +14,31 @@ window.open(url,'_blank')
 function voltarAoTopo(){
     window.scrollTo({ top: 0, behavior: 'smooth' })
 }
+
+function filtrar(){
+    const filtro = document.getElementById('filtro');
+    if(!filtro) return;
+    const valor = filtro.value;
+    const docinhos = document.getElementById('docinhos');
+    const bolos = document.getElementById('bolos');
+
+    if(!docinhos || !bolos) return;
+
+    if(valor === 'todos'){
+        docinhos.style.display = '';
+        bolos.style.display = '';
+        return;
+    }
+
+    if(valor === 'docinhos'){
+        docinhos.style.display = '';
+        bolos.style.display = 'none';
+        return;
+    }
+
+    if(valor === 'bolos'){
+        docinhos.style.display = 'none';
+        bolos.style.display = '';
+        return;
+    }
+}
